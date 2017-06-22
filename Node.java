006bea5c56e7c050a68b5ff1f34653c6281b04b2
Bharani Akella <bharani0akella@gmail.com>
2017-06-22 23:14:59 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,11 @@
+package interview;
+
+public class Node {
+int data;
+Node next;
+Node(int d){
+	data=d;
+	next=null;
+}
+
+}
---
+++ TheLink.java
@@ -0,0 +1,12 @@
+package interview;
+import java.util.Scanner;
+public class TheLink {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         LinkToolKit tool=new LinkToolKit();
+         tool.showOptions();
+        
+	}
+
+}
